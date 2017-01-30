e677e11d4a198f3056b9da6fa516a5abec02c195
daedalus404 <zack@ouhermans.com>
2017-01-30 17:04:57 -0600

    Added New Package

---
+++ StormShelterJava/src/Fona/ZZZTesting.java
@@ -0,0 +1,12 @@
+package Fona;
+import com.angryelectron.fona.Fona;
+
+public class ZZZTesting {
+	
+	public static void main(String[] args) {
+		System.out.println("Starting");
+		Fona fona = new Fona();
+		System.out.println("Finished");
+	}
+
+}
