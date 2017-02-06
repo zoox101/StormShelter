1cafbe1b80dc3683f2cc4536ad07cfc387abd70e
zoox101 <william@thebookers.net>
2017-02-06 16:27:07 -0600

    Added Fona Javadoc

--- StormShelterJava/src/Fona/ZZYTesting.java
+++ StormShelterJava/src/Fona/ZZYTesting.java
@@ -1,11 +1,16 @@
 package Fona;
 import com.angryelectron.fona.Fona;
+import com.angryelectron.fona.FonaException;
 
 public class ZZYTesting {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FonaException {
 		System.out.println("Starting");
+		
 		Fona fona = new Fona();
+		fona.open("myport", 115200);
+		
+		fona.batteryPercent();
 		
 		
 		System.out.println("Finished");
